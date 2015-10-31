9f4f0875c19e6f0d88fcf6160046e7dc38e0967b
Luke Lanchester <luke@lukelanchester.com>
2015-10-31 09:02:17 +0000

    Use composer for autoloading

--- functions.php
+++ functions.php
@@ -11,15 +11,15 @@ use Slab\Core\Http\Request;
  **/
 function slab_core_init() {
 
-	$autoloader = new Autoloader;
-	$autoloader->registerNamespace('Slab\Core', SLAB_CORE_DIR . 'src');
+	// $autoloader = new Autoloader;
+	// $autoloader->registerNamespace('Slab\Core', SLAB_CORE_DIR . 'src');
 
 	$slab = Application::instance();
 	$slab->singleton('Slab\Core\Application', $slab);
 	$slab->alias('Slab\Core\ContainerInterface', 'Slab\Core\Application');
 
-	$slab->singleton('Slab\Core\Autoloader', $autoloader);
-	$slab->alias('autoloader', 'Slab\Core\Autoloader');
+	// $slab->singleton('Slab\Core\Autoloader', $autoloader);
+	// $slab->alias('autoloader', 'Slab\Core\Autoloader');
 
 	$slab->singleton('Slab\Core\Http\RequestInterface', function(){
 		return Request::createFromGlobals();
